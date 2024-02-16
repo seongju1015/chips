0ba234962ff64ebf523de5f7acc80b1d5207a8ea
seongju1015 <gmrfkels1015@gmail.com>
2024-02-16 18:45:04 +0900

    초기 페이지 구현

--- chipspage/src/App.js
+++ chipspage/src/App.js
@@ -1,24 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
+import HomePage from './home';
+import chipsHomePage from './home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>  {/* Router로 감싸기 */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ chipspage/src/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const chipsHomePage = () => {
+
+    return(
+        <div>
+            this is test message
+        </div>
+    );
+}
+
+export default chipsHomePage;
