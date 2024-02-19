99865ed26c8ef1f55e364a8852c68d8d814bdadb
seongju1015 <gmrfkels1015@gmail.com>
2024-02-19 12:00:43 +0900

    리크루팅 페이지 연결

--- chipspage/src/App.js
+++ chipspage/src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes  } from 'react-router-dom';
 import HomePage from './home';
-import chipsHomePage from './home';
+import RecruitPage from './recruit';
 
 function App() {
   return (
     <Router>  {/* Router로 감싸기 */}
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path='/recruit' element={<RecruitPage/>}/>
       </Routes>
     </Router>
   );
---
+++ chipspage/src/recruit.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const recruit = () => {
+    return(
+        <div>this is recruit page</div>
+    )
+}
+
+export default recruit
